6c68d4a284a88d965bd76dd6d3b819a2ceff9527
bruce oberg <bruce@oberg.org>
2025-02-10 15:06:30 -0800

    default eastern time for easier sanity checking

--- main.py
+++ main.py
@@ -1652,7 +1652,7 @@ if True:
 		strDestDir = 'playground',
 		iterPagea = (
 			# SPageArgs(CCalOnlyPage, fmt=(18, 27), fmtCrop=None, strVariant = 'benjy orig', fMatchNumbers = True, fEliminationHints = False, fGroupDots = False),
-			SPageArgs(CCalElimPage, fmt=(20, 27), fmtCrop=None, strTz='US/Eastern'),
+			SPageArgs(CCalElimPage, fmt=(20, 27), fmtCrop=None, strTz='US/Pacific'),
 			SPageArgs(CCalElimPage, fmt=(20, 27), fmtCrop=None, strLocale='nl', strTz='Europe/Amsterdam'),
 			SPageArgs(CCalElimPage, fmt=(20, 27), fmtCrop=None, strTz='Asia/Qatar'),
 			SPageArgs(CCalElimPage, fmt=(20, 27), fmtCrop=None, strLocale='ja', strTz='Asia/Tokyo'),
