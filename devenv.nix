dcbb6c8e242c357c922b87f32136ded2109b5cd9
bruce oberg <bruce@oberg.org>
2025-12-14 12:58:33 -0800

    move to uv

--- devenv.nix
+++ devenv.nix
@@ -4,9 +4,19 @@
   languages.python =
   {
     enable = true;
+    version = "3.13";
     venv.enable = true;
-    venv.requirements = ./requirements.txt;
+
+    uv =
+    {
+      enable = true;
+      sync.enable = true;  # Auto-sync dependencies on direnv reload
+    };
   };
+
+  # Tell uv to use devenv's venv
+  
+  env.UV_PROJECT_ENVIRONMENT = "${config.devenv.root}/.devenv/state/venv";
 
   # See full reference at https://devenv.sh/reference/options/
 }
